dfccb1c41f8aa714629ffb3c5834d27469d5fe7c
MuhammadBugaje <muhammad.iliyasu2348@gmail.com>
2026-04-24 01:36:09 +0100

    fixes1

--- academics/utils.py
+++ academics/utils.py
@@ -1,15 +1,5 @@
 from io import BytesIO
 from django.http import HttpResponse
 from django.template.loader import get_template
-from xhtml2pdf import pisa
 from django.conf import settings
 import os
-
-def render_to_pdf(template_src, context_dict={}):
-    template = get_template(template_src)
-    html = template.render(context_dict)
-    result = BytesIO()
-    pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result)
-    if not pdf.err:
-        return HttpResponse(result.getvalue(), content_type='application/pdf')
-    return None
