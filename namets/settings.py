5f4d8dbb38cb5ec4afe1798b44b44a628f37784b
MuhammadBugaje <muhammad.iliyasu2348@gmail.com>
2026-05-02 09:47:39 +0100

    testing fix

--- namets/settings.py
+++ namets/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-dev-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'false'
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 #DEBUG = True
 
 # Application definition
